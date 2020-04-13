b80bb8199bf432176e6963547ea6af04a9d9dbb0
Luis Bernal <luis_bernal8010@yahoo.com>
2020-04-13 04:35:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,27 +1,18 @@
 def nyc_pigeon_organizer(data)
-  pigeon = {
-    "Theo" => {:color => ["purple", "grey"], :gender => ["male"], :lives => ["Subway"]},
-    "Peter Jr." => {:color => ["purple", "grey"], :gender => ["male"], :lives => ["Library"]},
-    "Lucky" => {:color => ["purple"], :gender => ["male"], :lives => ["Central Park"]},
-    "Ms. K" => {:color => ["grey", "white"], :gender => ["female"], :lives => ["Central Park"]},
-    "Queenie" => {:color => ["white", "brown"], :gender => ["female"], :lives => ["Subway"]},
-    "Andrew" => {:color => ["white"], :gender => ["male"], :lives => ["City Hall"]},
-    "Alex" => {:color => ["white", "brown"], :gender => ["male"], :lives => ["Central Park"]},
-  }
-
+  pigeon_list = {}
+  data.each do |color_gender_lives, value|
+    value.each do |stats, all_names|
+      all_names.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][color_gender_lives] == nil
+          pigeon_list[name][color_gender_lives] = []
+        end
+        pigeon_list[name][color_gender_lives].push(stats.to_s)
+      end
+    end
+  end
+  pigeon_list
 end
-
-alternate = {
-  :color => {
-:brown => ["Luca"],
-:black => ["Lola"],
-},
-:gender => {
-:male => ["Luca"],
-:female => ["Lola"]
-},
-:lives => {
-"Central Park" => ["Lola"],
-"Library" => ["Luca"]
-}
-}
+end
